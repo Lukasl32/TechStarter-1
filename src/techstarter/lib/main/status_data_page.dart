3ef6b488199f9560de6cc82e9bc89fea9ac9e643
DavidCZ2051 <david.vobruba@centrum.cz>
2025-03-09 21:49:49 +0100

    Upraven design status stránky

--- src/techstarter/lib/main/status_data_page.dart
+++ src/techstarter/lib/main/status_data_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 class StatusDataPage extends StatefulWidget {
   const StatusDataPage({super.key});
@@ -14,27 +15,43 @@ class _StatusDataPageState extends State<StatusDataPage> {
       padding: const EdgeInsets.all(10.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
-        spacing: 40,
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              FirstCard(commandCode: "XXXXXXXXXXX"),
-              SecondCard(
-                defectsMachine: 50,
-                defectsOperator: 35,
+          Flexible(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  FirstCard(commandCode: "XXXXXXXXXXX"),
+                  const SizedBox(height: 8),
+                  SecondCard(
+                    defectsMachine: 50,
+                    defectsOperator: 35,
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              ThirdCard(title: "Žádané kusy:", value: 650),
-              ThirdCard(title: "Vyrobené kusy:", value: 650),
-              ThirdCard(title: "Celkem vyrobené kusy:", value: 650),
-              ThirdCard(title: "Vyrobené za 24h:", value: 650),
-            ],
-          )
+          Flexible(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ThirdCard(
+                titles: [
+                  "Žádané kusy:",
+                  "Vyrobené kusy:",
+                  "Celkem vyrobené kusy:",
+                  "Kusy vyrobené strojem:"
+                ],
+                values: [
+                  650,
+                  650,
+                  650,
+                  650,
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
@@ -59,19 +76,24 @@ class _FirstCardState extends State<FirstCard> {
         padding: const EdgeInsets.all(12.0),
         child: Column(
           children: [
-            Text("Aktuálně vyráběný prvek:",
-                style: TextStyle(
-                  fontSize: (30),
-                  fontWeight: FontWeight.bold,
-                ),
-                overflow: TextOverflow.visible,
-                textAlign: TextAlign.center),
-            Text(widget.commandCode,
-                style: TextStyle(
-                  fontSize: (widget.commandCode.length < 20 ? 30 : 25),
-                ),
-                overflow: TextOverflow.visible,
-                textAlign: TextAlign.center),
+            Text(
+              "Aktuálně vyráběný prvek:",
+              style: TextStyle(
+                fontSize: (30),
+                fontWeight: FontWeight.bold,
+              ),
+              overflow: TextOverflow.visible,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              widget.commandCode,
+              style: TextStyle(
+                fontSize: (widget.commandCode.length < 20 ? 30 : 25),
+              ),
+              overflow: TextOverflow.visible,
+              textAlign: TextAlign.center,
+            ),
           ],
         ),
       ),
@@ -81,7 +103,11 @@ class _FirstCardState extends State<FirstCard> {
 
 // Kusy označené chybou
 class SecondCard extends StatefulWidget {
-  const SecondCard({super.key, required this.defectsMachine, required this.defectsOperator});
+  const SecondCard({
+    super.key,
+    required this.defectsMachine,
+    required this.defectsOperator,
+  });
 
   final int defectsMachine;
   final int defectsOperator;
@@ -95,54 +121,124 @@ class _SecondCardState extends State<SecondCard> {
   Widget build(BuildContext context) {
     return Card(
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: Column(
-              children: [
-                Text("Defekty STROJ:",
-                    style: TextStyle(
-                      fontSize: (30),
-                      fontWeight: FontWeight.bold,
-                    ),
-                    overflow: TextOverflow.visible,
-                    textAlign: TextAlign.center),
-                Text(widget.defectsMachine.toString(),
-                    style: TextStyle(
-                      fontSize: (widget.defectsMachine.toString().length < 20 ? 30 : 25),
-                    ),
-                    overflow: TextOverflow.visible,
-                    textAlign: TextAlign.center),
-
-                Text("Defekty OPERÁTOR:",
-                    style: TextStyle(
-                      fontSize: (30),
-                      fontWeight: FontWeight.bold,
-                    ),
-                    overflow: TextOverflow.visible,
-                    textAlign: TextAlign.center),
-                Text(widget.defectsOperator.toString(),
-                    style: TextStyle(
-                      fontSize: (widget.defectsOperator.toString().length < 20 ? 30 : 25),
-                    ),
-                    overflow: TextOverflow.visible,
-                    textAlign: TextAlign.center),
-              ],
-            ),
+          Column(
+            children: [
+              Text(
+                "Defekty STROJ",
+                style: TextStyle(
+                  fontSize: (30),
+                  fontWeight: FontWeight.bold,
+                ),
+                overflow: TextOverflow.visible,
+                textAlign: TextAlign.center,
+              ),
+              SfRadialGauge(
+                axes: [
+                  RadialAxis(
+                    maximum: 150,
+                    pointers: [
+                      NeedlePointer(
+                        value: widget.defectsMachine.toDouble(),
+                      ),
+                    ],
+                    ranges: [
+                      GaugeRange(
+                        startValue: 0,
+                        endValue: 50,
+                        color: Colors.red,
+                      ),
+                      GaugeRange(
+                        startValue: 50,
+                        endValue: 100,
+                        color: Colors.orange,
+                      ),
+                      GaugeRange(
+                        startValue: 100,
+                        endValue: 150,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Text(
+                widget.defectsMachine.toString(),
+                style: TextStyle(
+                  fontSize:
+                      (widget.defectsMachine.toString().length < 20 ? 30 : 25),
+                ),
+                overflow: TextOverflow.visible,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+          Column(
+            children: [
+              Text(
+                "Defekty OPERÁTOR",
+                style: TextStyle(
+                  fontSize: (30),
+                  fontWeight: FontWeight.bold,
+                ),
+                overflow: TextOverflow.visible,
+                textAlign: TextAlign.center,
+              ),
+              SfRadialGauge(
+                axes: [
+                  RadialAxis(
+                    maximum: 150,
+                    pointers: [
+                      NeedlePointer(
+                        value: widget.defectsOperator.toDouble(),
+                      ),
+                    ],
+                    ranges: [
+                      GaugeRange(
+                        startValue: 0,
+                        endValue: 50,
+                        color: Colors.red,
+                      ),
+                      GaugeRange(
+                        startValue: 50,
+                        endValue: 100,
+                        color: Colors.orange,
+                      ),
+                      GaugeRange(
+                        startValue: 100,
+                        endValue: 150,
+                        color: Colors.green,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Text(
+                widget.defectsOperator.toString(),
+                style: TextStyle(
+                  fontSize:
+                      (widget.defectsOperator.toString().length < 20 ? 30 : 25),
+                ),
+                overflow: TextOverflow.visible,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+            ],
           ),
         ],
       ),
     );
-    ;
   }
 }
 
 // Žádané kusy | Vyrobené kusy | Celkem vyrobené kusy | Kusy vyrobené strojem
 class ThirdCard extends StatefulWidget {
-  const ThirdCard({super.key, required this.title, required this.value});
+  const ThirdCard({super.key, required this.titles, required this.values});
 
-  final String title;
-  final int value;
+  final List<String> titles;
+  final List<int> values;
 
   @override
   State<ThirdCard> createState() => _ThirdCardState();
@@ -155,23 +251,32 @@ class _ThirdCardState extends State<ThirdCard> {
       child: Padding(
         padding: const EdgeInsets.all(12.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(widget.title,
+            for (int i = 0; i < widget.titles.length; i++) ...[
+              Text(
+                widget.titles[i],
                 style: TextStyle(
-                  fontSize: (widget.title.length < 20 ? 25 : 20),
+                  fontSize: (widget.titles[i].length < 20 ? 25 : 20),
                   fontWeight: FontWeight.bold,
                 ),
                 overflow: TextOverflow.visible,
-                textAlign: TextAlign.center),
-            Text(widget.value.toString(),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 5),
+              Text(
+                widget.values[i].toString(),
                 style: TextStyle(
                   fontSize: (30),
                 ),
                 overflow: TextOverflow.visible,
-                textAlign: TextAlign.center),
+                textAlign: TextAlign.center,
+              ),
+              const Divider(),
+            ],
           ],
         ),
       ),
-    );;
+    );
   }
 }
