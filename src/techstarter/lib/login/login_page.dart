773b90bccabdec5b6410b0934801f442f3a89d2d
DavidCZ2051 <david.vobruba@centrum.cz>
2025-03-11 13:03:02 +0100

    Příprava na globální přihlašování

--- src/techstarter/lib/login/login_page.dart
+++ src/techstarter/lib/login/login_page.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:gui/database_controller.dart';
-import 'package:gui/globals.dart' as globals;
 import 'package:gui/main/main_screen.dart';
 import 'package:gui/main.dart';
-import 'package:flutter_libserialport/flutter_libserialport.dart';
-import 'dart:typed_data';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -14,47 +10,6 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  @override
-  void initState() {
-    super.initState();
-    //readCard(); //TODO: zapnout kvůli přikládce karet
-  }
-
-  void readCard() async {
-    await getCard();
-  }
-
-  Future getCard() async {
-    SerialPort serialPort = SerialPort("/dev/ttyAMA0");
-    serialPort.openRead();
-    Uint8List data = Uint8List(0);
-    while (true) {
-      // Čtení dat z RFID čtečky, dokud se nepřiloží karta
-      data = serialPort.read(12, timeout: 100);
-      await Future.delayed(
-        const Duration(milliseconds: 200),
-      ); // Asynchronní čekání, ať neblokuje UI thread
-      if (data.isNotEmpty) {
-        break;
-      }
-    }
-    // 12 znakový hex string reprezentující ID karty - sloupec rfid v tabulce operators
-    String cardId = String.fromCharCodes(data);
-    print(cardId);
-    // Přihlášení operátora podle RFID karty
-    globals.operator = Operator.fetchByRfid(cardId);
-    // Pokud se podařilo operátora v DB najít, přesměruje na hlavní obrazovku
-    // (Proměnná mounted je jenom Flutter věc)
-    if (globals.operator != null && mounted) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => MainScreen(),
-        ),
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
--- src/techstarter/lib/main.dart
+++ src/techstarter/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:gui/database_controller.dart';
 import 'package:gui/login/login_screen.dart';
+import 'package:flutter_libserialport/flutter_libserialport.dart';
+import 'dart:typed_data';
+import 'package:gui/globals.dart' as globals;
+import 'package:gui/main/main_screen.dart';
 
 const Color baseColor = Color.fromARGB(255, 145, 192, 33);
 const Color baseColor1 = Color.fromARGB(255, 66, 66, 66);
@@ -21,20 +25,55 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp>
-    with SingleTickerProviderStateMixin {
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
   late AnimationController _controller;
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(vsync: this);
+    readCard(); //TODO: zapnout kvůli přikládce karet
   }
 
   @override
   void dispose() {
     _controller.dispose();
     super.dispose();
+  }
+
+  void readCard() async {
+    await getCard();
+  }
+
+  Future getCard() async {
+    SerialPort serialPort = SerialPort("/dev/ttyAMA0");
+    serialPort.openRead();
+    Uint8List data = Uint8List(0);
+    while (true) {
+      // Čtení dat z RFID čtečky, dokud se nepřiloží karta
+      data = serialPort.read(12, timeout: 100);
+      await Future.delayed(
+        const Duration(milliseconds: 300),
+      ); // Asynchronní čekání, ať neblokuje UI thread
+      if (data.isNotEmpty) {
+        break;
+      }
+    }
+    // 12 znakový hex string reprezentující ID karty - sloupec rfid v tabulce operators
+    String cardId = String.fromCharCodes(data);
+    print(cardId);
+    // Přihlášení operátora podle RFID karty
+    globals.operator = Operator.fetchByRfid(cardId);
+    // Pokud se podařilo operátora v DB najít, přesměruje na hlavní obrazovku
+    // (Proměnná mounted je jenom Flutter věc)
+    if (globals.operator != null && mounted) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => MainScreen(),
+        ),
+      );
+    }
   }
 
   @override
